76b740ff655ec9d00e542992fcad27f49e0dbd90
Riley <19radleriley@gmail.com>
2023-02-01 20:25:27 -0600

    Finished most of the code for the navbar at mobile view

--- js/navbar.js
+++ js/navbar.js
@@ -42,14 +42,23 @@ $(() => {
     $("#hamburger").click(() => {
         var hidden = $("nav").attr("class");
 
-        // Not hidden
-        if (hidden.length == 0) {
+        // Not hidden so hide it
+        if (hidden == "nav-visible") {
             $("nav").attr("class", "nav-hidden");
+            $("#backdrop-filter").fadeOut();
         }
-        // Hidden
+        // Hidden so show it
         else {
-            $("nav").attr("class", "");
+            $("nav").attr("class", "nav-visible");
+            $("#backdrop-filter").fadeIn();
         }
+    });
+
+    // If someone clicks on the outside of 
+    // the nav when in mobile view, exit the nav
+    $("#backdrop-filter").click(() => {
+        $("nav").attr("class", "nav-hidden");
+        $("#backdrop-filter").fadeOut();
     });
 });
 

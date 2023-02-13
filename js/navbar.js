e1e0faeb36f06841ac53838c6fd70a2c91ba32bc
Riley <19radleriley@gmail.com>
2023-02-13 12:34:26 -0600

    Refactor done

--- js/navbar.js
+++ js/navbar.js
@@ -1,47 +1,5 @@
 $(() => {
 
-    // Go to the home page when the logo is clicked
-    $("#logo").click(() => {
-        clearCurrentNavigation(); 
-        $("nav").attr("class", "nav-hidden");
-
-        $("main").fadeToggle(100, () => {
-            // displayHomePage();
-            $("#home").fadeToggle(0, () => {
-                displayHomePage();
-            });
-        });
-    });
-
-    // Update the nav-links and pages on click
-    $(".nav-link").click(event => {
-    
-        var classes = event.target.classList;
-
-        // This is not the current class, update it 
-        if (!classes.contains("current")) {
-
-            clearCurrentNavigation();
-            event.target.classList.add("current");
-
-            switch (event.target.id) {
-                case "work-link":
-                    $("#work").attr("aria-hidden", "false");                    
-                    break;
-                case "about-link":
-                    $("#about").attr("aria-hidden", "false");                    
-                    break;
-                case "contact-link":
-                    $("#contact").attr("aria-hidden", "false");                    
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        // Else this is already the current...Do nothing
-    });
-
     // Nav bar toggling 
     $("#hamburger").click(() => {
         var hidden = $("nav").attr("class");
@@ -65,8 +23,3 @@ $(() => {
         $("#backdrop-filter").fadeOut();
     });
 });
-
-function clearCurrentNavigation() {
-    $('.page[aria-hidden="false"]').attr("aria-hidden", "true");
-    $(".nav-link.current").attr("class", "nav-link hoverable");
-}

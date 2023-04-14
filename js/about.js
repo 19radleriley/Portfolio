33bef647263d44f0d34c9b4c177c1468a59619d0
Riley <19radleriley@gmail.com>
2023-04-14 01:16:12 -0500

    Testing something on mobile

--- js/about.js
+++ js/about.js
@@ -1,21 +1,19 @@
 $(() => {
     $(".history-item").on("click", function(target) {
         // 812 is the magical number where the history grid breaks to two columns
-        // if (window.innerWidth >= 812) {
-            $("#history-grid").slideToggle(500, () => {
-                var clone = $(this).clone();
-                $(".history").append(clone.addClass("expanded").css("display", "none"));
-                clone.slideToggle(500);
+        $("#history-grid").fadeToggle(300, () => {
+            var clone = $(this).clone();
+            $(".history").append(clone.addClass("expanded").css("display", "none"));
+            clone.fadeToggle(300);
 
-                var moreInfo = $(".history-item.expanded");
+            var moreInfo = $(".history-item.expanded");
 
-                moreInfo.on("click", () => {
-                    moreInfo.slideToggle(500, () => {
-                        moreInfo.remove();
-                        $("#history-grid").slideToggle(500);
-                    });
-                })
-            });
-        // }
+            moreInfo.on("click", () => {
+                moreInfo.fadeToggle(300, () => {
+                    moreInfo.remove();
+                    $("#history-grid").fadeToggle(300);
+                });
+            })
+        });
     });
 });

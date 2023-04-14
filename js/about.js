8ceabfac8b6589447b6858083bff0d81bfa37148
Riley <19radleriley@gmail.com>
2023-04-13 23:33:36 -0500

    Want to see the functionality on mobile

--- js/about.js
+++ js/about.js
@@ -2,17 +2,17 @@ $(() => {
     $(".history-item").on("click", function(target) {
         // 812 is the magical number where the history grid breaks to two columns
         // if (window.innerWidth >= 812) {
-            $("#history-grid").fadeToggle(250, () => {
+            $("#history-grid").slideToggle(500, () => {
                 var clone = $(this).clone();
                 $(".history").append(clone.addClass("expanded").css("display", "none"));
-                clone.fadeToggle(250);
+                clone.slideToggle(500);
 
                 var moreInfo = $(".history-item.expanded");
 
                 moreInfo.on("click", () => {
-                    moreInfo.fadeToggle(250, () => {
+                    moreInfo.slideToggle(500, () => {
                         moreInfo.remove();
-                        $("#history-grid").fadeToggle(500);
+                        $("#history-grid").slideToggle(500);
                     });
                 })
             });

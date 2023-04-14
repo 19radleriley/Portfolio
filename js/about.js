abf7fa3a76ef9ce70ed1d644aeb71c4c98907b2b
Riley <19radleriley@gmail.com>
2023-04-14 02:18:28 -0500

    Finishing for the night. I mostly have the history cards working, just need to fill in the information now.

--- js/about.js
+++ js/about.js
@@ -3,17 +3,17 @@ $(() => {
         // 812 is the magical number where the history grid breaks to two columns
         $("#history-grid").fadeToggle(300, () => {
             var clone = $(this).clone();
-            $(".history").append(clone.addClass("expanded").css("display", "none"));
+            $("#history").append(clone.addClass("expanded").css("display", "none"));
+            $(".history-item.expanded .thumbnail").removeClass("hoverable");
             clone.fadeToggle(300);
-
             var moreInfo = $(".history-item.expanded");
 
-            moreInfo.on("click", () => {
+            $(".history-item.expanded .more-info").append($('<button class="back hoverable">back</button>').on("click", () => {
                 moreInfo.fadeToggle(300, () => {
                     moreInfo.remove();
                     $("#history-grid").fadeToggle(300);
                 });
-            })
+            }));
         });
     });
 });

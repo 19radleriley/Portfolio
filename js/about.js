1d635bc1042ed2c0f3c209c81c812f23fd18dbc7
Riley <19radleriley@gmail.com>
2023-04-15 00:06:14 -0500

    Added a way cooler functionality for the history section

--- js/about.js
+++ js/about.js
@@ -1,20 +1,76 @@
 $(() => {
-    $(".history-item").on("click", function(target) {
-        // 812 is the magical number where the history grid breaks to two columns
-        $("#history-grid").fadeToggle(300, () => {
-            var clone = $(this).clone();
-            $("#history").append(clone.addClass("expanded").css("display", "none"));
-            $(".history-item.expanded .thumbnail-title").remove();
-            $(".history-item.expanded .thumbnail").removeClass("hoverable");
-            clone.fadeToggle(300);
-            var moreInfo = $(".history-item.expanded");
 
-            $(".history-item.expanded .more-info").append($('<button class="back hoverable">back</button>').on("click", () => {
-                moreInfo.fadeToggle(300, () => {
-                    moreInfo.remove();
-                    $("#history-grid").fadeToggle(300);
-                });
-            }));
+    $(".history-item").on("click", (event) => {
+        var rect = event.target.getBoundingClientRect();
+        toCenter(event.target, rect.top, rect.left, rect.width, rect.height);
+
+    });
+
+});
+
+function toCenter(elem, top, left, width, height) {
+    var clone = $(elem).clone().attr("class", "");
+    var info = $(elem.nextElementSibling).clone();
+    $(info[0].firstElementChild).append($('<button class="close hoverable">close</button>'));
+    var popup = $('<div class=history-popup></div>')
+                    .css("width", `${width}px`)
+                    .css("height", `${height}px`)
+                    .css("top", `${top}px`)
+                    .css("left", `${left}px`)
+                    .append(clone)
+                    .append(info);
+
+    $("body").append(popup);
+
+    popup.toggle(0, () => {
+
+        var numColumns = getComputedStyle(document.querySelector("#history-grid"))
+                            .getPropertyValue("grid-template-columns").split(" ").length;
+        var scaleFactor = numColumns == 1 ? 1 : 1.5;
+
+        $(elem).css("opacity", 0);
+        popup.css("transform", "translate(-50%, -50%)")
+             .css("top", "50%")
+             .css("left", "50%");
+
+        setTimeout(() => {
+            popup.css("width", `${Math.round(width * scaleFactor)}px`)
+                .css("height", `${Math.round(height * scaleFactor)}px`);
+            info.slideToggle(500);
+            // Hide everything behind 
+            $("#about > *").css("opacity", 0).css("pointer-events", "none");
+        }, 250);
+
+
+        // Event to close the popup
+        $("body").on("click", (event) => {
+            if (event.target == document.body ||
+                event.target == $(".history-popup .close")[0] ||
+                event.target == $("main")[0]) {
+                info.toggle();
+                toOriginal(elem, width, height);
+            }
         });
     });
-});
+}
+
+function toOriginal(orig, width, height) {
+    $(".history-popup")
+        .css("width", `${width}px`)
+        .css("height", `${height}px`)
+
+    setTimeout(() => {
+        $(".history-popup")
+            .css("top", `${orig.getBoundingClientRect().top}px`)
+            .css("left", `${orig.getBoundingClientRect().left}px`)
+            .css("transform", "");
+
+        $("#about > *").css("opacity", 1).css("pointer-events", "all");
+        $(".history-popup").css("opacity", 0);
+        $(orig).css("opacity", 1);
+        setTimeout(() => {
+            $(".history-popup").remove();
+        }, 250);
+    }, 250);
+
+}

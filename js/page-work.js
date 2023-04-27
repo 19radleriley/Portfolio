a4c859980d1070751219bd770abf7204f4ec3b0c
Riley <19radleriley@gmail.com>
2023-04-27 13:29:55 -0500

    Got to point where the filter is updating. Now would just need to refactor the masonry grid to be more generic and add the photos

--- js/page-work.js
+++ js/page-work.js
@@ -36,37 +36,83 @@ $(() => {
     // Initially show the client work
     addMasonryGridItems(clientWork, "#client-work");
 
-    $("#client-title").click(updateWorkType);
-    $("#personal-title").click(updateWorkType);
+    $(".filter.current").on("click", toggleWorkFilterDropdown);
+    $("#filter-dropdown > *").on("click", updateWorkFilter);
+
+    // $("#client-title").click(updateWorkType);
+    // $("#personal-title").click(updateWorkType);
 
     $(".gallery").click(closeGallery);
 });
 
-function updateWorkType() {
-    if (!this.className.includes("selected")) {
-        $(".work-type.selected").attr("class", "work-type hoverable");
-        this.className = "work-type hoverable selected";
+function updateWorkFilter() {
+    console.dir(this);
+    var temp = this.innerText;
+    this.innerText = $(".filter.current").text().trim();
+    $(".filter.current").text(temp);
+    $(".filter.current").append($(dropdownArrow));
 
-        // Hide / show the corresponding work section
-        if (this.id == "client-title") {
-            $("#client-work").css("display", "grid");
-            $("#personal-work").css("display", "none");
+    // Rehide the dropdown
+    updateFilterClickOff();
+    $("#filter-dropdown").css("display", "none");
+}
 
-            addMasonryGridItems(clientWork, "#client-work");
-            $("#personal-work > *").remove();
-        }
-        else {
-            $("#client-work").css("display", "none");
-            $("#personal-work").css("display", "grid");
+function toggleWorkFilterDropdown() {
+    updateFilterClickOff();
+    $("#filter-dropdown").slideToggle(250, "swing");
+}
 
-            addMasonryGridItems(personalWork, "#personal-work");
-                 // Add the functionality for gallery mode
-            $("#client-work > *").remove();
+function updateFilterClickOff() {
+    var dropdown = $("#filter-dropdown");
 
-            $(`#personal-work .masonry-grid-item`).click(function() {
-                var orientation = this.className.includes("landscape") ? "landscape" : "portrait";
-                startGallery(this.src, this.id, orientation);
-            });
-        }
+    // If the dropdown is not showing and we are about to show it
+    if (dropdown.css("display") == "none") {
+        $(window).on("click", clickOffFilter);
+    }
+    else {
+        $(window).off("click", clickOffFilter);
     }
 }
+
+function clickOffFilter(e) {
+    if (!e.target.closest("#work-filter")) {
+        toggleWorkFilterDropdown();
+    }
+}
+
+// function updateWorkType() {
+//     if (!this.className.includes("selected")) {
+//         $(".work-type.selected").attr("class", "work-type hoverable");
+//         this.className = "work-type hoverable selected";
+
+//         // Hide / show the corresponding work section
+//         if (this.id == "client-title") {
+//             $("#client-work").css("display", "grid");
+//             $("#personal-work").css("display", "none");
+
+//             addMasonryGridItems(clientWork, "#client-work");
+//             $("#personal-work > *").remove();
+//         }
+//         else {
+//             $("#client-work").css("display", "none");
+//             $("#personal-work").css("display", "grid");
+
+//             addMasonryGridItems(personalWork, "#personal-work");
+//                  // Add the functionality for gallery mode
+//             $("#client-work > *").remove();
+
+//             $(`#personal-work .masonry-grid-item`).click(function() {
+//                 var orientation = this.className.includes("landscape") ? "landscape" : "portrait";
+//                 startGallery(this.src, this.id, orientation);
+//             });
+//         }
+//     }
+// }
+
+const dropdownArrow = `
+<svg class="dropdown-arrow" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 50 50"
+style="enable-background:new 0 0 50 50;" xml:space="preserve">
+<polygon points="40,23.8 40,17.5 10,17.5 10,23.8 25,42.5 " />
+</svg>
+`

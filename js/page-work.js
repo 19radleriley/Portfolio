b3ecc64ca0762fb9da67730a96965e1960a74b1f
Riley Radle <61852965+19radleriley@users.noreply.github.com>
2026-04-23 09:43:32 -0500

    Add new personal work item to MasonryGrid

--- js/page-work.js
+++ js/page-work.js
@@ -95,6 +95,9 @@ const work = [
                          .setAltText("This is a screenshot of a simple web app I created when I was getting started in web development. The thumnail depicts the home page of a hypothetical site where I can sell my art with several item categories (ex. 'portraits' 'shirts' etc)."),
 
     // ==================== Personal ====================
+    new MasonryGridItem().setImage("https://img.vsco.co/375c41/273964258/6988a88c51c31bb58b9f7c7d/vsco_020826.jpg")
+                         .addTag(workTypes.personal.tag)
+                         .setAltText("A picture of a zine about coffee."),
     new MasonryGridItem().setImage("https://img.vsco.co/375c41/273964258/6972f7619c17524c80e5bd55/CafeConLeche.png")
                          .addTag(workTypes.personal.tag)
                          .setAltText("A digital painting of a coffee on a wooden table."),
